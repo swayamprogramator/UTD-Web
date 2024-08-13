e7a70e9b6986e37887e45c8240f18a7611463e96
Harsh Sharma <student3.ai@csvtu.ac.in>
2024-08-13 20:15:12 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Your JavaScript code goes here
+});
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === 'block') {
+                openDropdown.style.display = 'none';
+            }
+        }
+    }
+}
